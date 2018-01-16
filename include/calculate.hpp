e7fa51373af6bd9534b09597b03f53a2b10b2489
Alberto Lorenzo Márquez <alorenzo.md@gmail.com>
2018-01-16 12:32:26 +0100

    Restored default parser class

--- include/calculate.hpp
+++ include/calculate.hpp
@@ -13,192 +13,128 @@ class Parser : public BaseParser<double> {};
 class DefaultParser : public Parser {
 public:
     DefaultParser() {
-        set<Constant>("pi", 3.14159265358979323846);
-        set<Constant>("e", 2.71828182845904523536);
-        set<Constant>("phi", 1.61803398874989484820);
-        set<Constant>("gamma", 0.57721566490153286060);
+        constants.insert({
+            {"pi", 3.14159265358979323846},
+            {"e", 2.71828182845904523536},
+            {"phi", 1.61803398874989484820},
+            {"gamma", 0.57721566490153286060}
+        });
 
-        set<Function>("id",
-            [](Type x) noexcept { return x; }
-        );
-        set<Function>("neg",
-            [](Type x) noexcept { return -x; }
-        );
-        set<Function>("inv",
-            [](Type x) noexcept { return Type{1} / x; }
-        );
+        functions.insert({
+            {"id", [](Type x) noexcept { return x; }},
+            {"neg", [](Type x) noexcept { return -x; }},
+            {"inv", [](Type x) noexcept { return Type{1} / x; }},
+            {"fabs", static_cast<Type(*)(Type)>(std::fabs)},
+            {"abs", static_cast<Type(*)(Type)>(std::abs)},
+            {"fma", static_cast<Type(*)(Type, Type, Type)>(std::fma)},
+            {"copysign", static_cast<Type(*)(Type, Type)>(std::copysign)},
+            {"nextafter", static_cast<Type(*)(Type, Type)>(std::nextafter)},
+            {"fdim", static_cast<Type(*)(Type, Type)>(std::fdim)},
+            {"fmax", static_cast<Type(*)(Type, Type)>(std::fmax)},
+            {"fmin", static_cast<Type(*)(Type, Type)>(std::fmin)},
+            {"fdim", static_cast<Type(*)(Type, Type)>(std::fdim)},
+            {"fmax", static_cast<Type(*)(Type, Type)>(std::fmax)},
+            {"fmin", static_cast<Type(*)(Type, Type)>(std::fmin)},
+            {"ceil", static_cast<Type(*)(Type)>(std::ceil)},
+            {"floor", static_cast<Type(*)(Type)>(std::floor)},
+            {"fmod", static_cast<Type(*)(Type, Type)>(std::fmod)},
+            {"trunc", static_cast<Type(*)(Type)>(std::trunc)},
+            {"round", static_cast<Type(*)(Type)>(std::round)},
+            {"rint", static_cast<Type(*)(Type)>(std::rint)},
+            {"nearbyint", static_cast<Type(*)(Type)>(std::nearbyint)},
+            {"remainder", static_cast<Type(*)(Type, Type)>(std::remainder)},
+            {"pow", static_cast<Type(*)(Type, Type)>(std::pow)},
+            {"sqrt", static_cast<Type(*)(Type)>(std::sqrt)},
+            {"cbrt", static_cast<Type(*)(Type)>(std::cbrt)},
+            {"hypot", static_cast<Type(*)(Type, Type)>(std::hypot)},
+            {"exp", static_cast<Type(*)(Type)>(std::exp)},
+            {"expm1", static_cast<Type(*)(Type)>(std::expm1)},
+            {"exp2", static_cast<Type(*)(Type)>(std::exp2)},
+            {"log", static_cast<Type(*)(Type)>(std::log)},
+            {"log10", static_cast<Type(*)(Type)>(std::log10)},
+            {"log1p", static_cast<Type(*)(Type)>(std::log1p)},
+            {"log2", static_cast<Type(*)(Type)>(std::log2)},
+            {"logb", static_cast<Type(*)(Type)>(std::logb)},
+            {"sin", static_cast<Type(*)(Type)>(std::sin)},
+            {"cos", static_cast<Type(*)(Type)>(std::cos)},
+            {"tan", static_cast<Type(*)(Type)>(std::tan)},
+            {"asin", static_cast<Type(*)(Type)>(std::asin)},
+            {"acos", static_cast<Type(*)(Type)>(std::acos)},
+            {"atan", static_cast<Type(*)(Type)>(std::atan)},
+            {"atan2", static_cast<Type(*)(Type, Type)>(std::atan2)},
+            {"sinh", static_cast<Type(*)(Type)>(std::sinh)},
+            {"cosh", static_cast<Type(*)(Type)>(std::cosh)},
+            {"tanh", static_cast<Type(*)(Type)>(std::tanh)},
+            {"asinh", static_cast<Type(*)(Type)>(std::asinh)},
+            {"acosh", static_cast<Type(*)(Type)>(std::acosh)},
+            {"atanh", static_cast<Type(*)(Type)>(std::atanh)},
+            {"erf", static_cast<Type(*)(Type)>(std::erf)},
+            {"erfc", static_cast<Type(*)(Type)>(std::erfc)},
+            {"tgamma", static_cast<Type(*)(Type)>(std::tgamma)},
+            {"lgamma", static_cast<Type(*)(Type)>(std::lgamma)}
+        });
 
-        set<Function>("fabs",
-            static_cast<Type(*)(Type)>(std::fabs)
-        );
-        set<Function>("abs",
-            static_cast<Type(*)(Type)>(std::abs)
-        );
-        set<Function>("fma",
-            static_cast<Type(*)(Type, Type, Type)>(std::fma)
-        );
-        set<Function>("copysign",
-            static_cast<Type(*)(Type, Type)>(std::copysign)
-        );
-        set<Function>("nextafter",
-            static_cast<Type(*)(Type, Type)>(std::nextafter)
-        );
-
-        set<Function>("fdim",
-            static_cast<Type(*)(Type, Type)>(std::fdim)
-        );
-        set<Function>("fmax",
-            static_cast<Type(*)(Type, Type)>(std::fmax)
-        );
-        set<Function>("fmin",
-            static_cast<Type(*)(Type, Type)>(std::fmin)
-        );
-
-        set<Function>("ceil",
-            static_cast<Type(*)(Type)>(std::ceil)
-        );
-        set<Function>("floor",
-            static_cast<Type(*)(Type)>(std::floor)
-        );
-        set<Function>("fmod",
-            static_cast<Type(*)(Type, Type)>(std::fmod)
-        );
-        set<Function>("trunc",
-            static_cast<Type(*)(Type)>(std::trunc)
-        );
-        set<Function>("round",
-            static_cast<Type(*)(Type)>(std::round)
-        );
-        set<Function>("rint",
-            static_cast<Type(*)(Type)>(std::rint)
-        );
-        set<Function>("nearbyint",
-            static_cast<Type(*)(Type)>(std::nearbyint)
-        );
-        set<Function>("remainder",
-            static_cast<Type(*)(Type, Type)>(std::remainder)
-        );
-
-        set<Function>("pow",
-            static_cast<Type(*)(Type, Type)>(std::pow)
-        );
-        set<Function>("sqrt",
-            static_cast<Type(*)(Type)>(std::sqrt)
-        );
-        set<Function>("cbrt",
-            static_cast<Type(*)(Type)>(std::cbrt)
-        );
-        set<Function>("hypot",
-            static_cast<Type(*)(Type, Type)>(std::hypot)
-        );
-        set<Function>("exp",
-            static_cast<Type(*)(Type)>(std::exp)
-        );
-        set<Function>("expm1",
-            static_cast<Type(*)(Type)>(std::expm1)
-        );
-        set<Function>("exp2",
-            static_cast<Type(*)(Type)>(std::exp2)
-        );
-        set<Function>("log",
-            static_cast<Type(*)(Type)>(std::log)
-        );
-        set<Function>("log10",
-            static_cast<Type(*)(Type)>(std::log10)
-        );
-        set<Function>("log1p",
-            static_cast<Type(*)(Type)>(std::log1p)
-        );
-        set<Function>("log2",
-            static_cast<Type(*)(Type)>(std::log2)
-        );
-        set<Function>("logb",
-            static_cast<Type(*)(Type)>(std::logb)
-        );
-
-        set<Function>("sin",
-            static_cast<Type(*)(Type)>(std::sin)
-        );
-        set<Function>("cos",
-            static_cast<Type(*)(Type)>(std::cos)
-        );
-        set<Function>("tan",
-            static_cast<Type(*)(Type)>(std::tan)
-        );
-        set<Function>("asin",
-            static_cast<Type(*)(Type)>(std::asin)
-        );
-        set<Function>("acos",
-            static_cast<Type(*)(Type)>(std::acos)
-        );
-        set<Function>("atan",
-            static_cast<Type(*)(Type)>(std::atan)
-        );
-        set<Function>("atan2",
-            static_cast<Type(*)(Type, Type)>(std::atan2)
-        );
-
-        set<Function>("sinh",
-            static_cast<Type(*)(Type)>(std::sinh)
-        );
-        set<Function>("cosh",
-            static_cast<Type(*)(Type)>(std::cosh)
-        );
-        set<Function>("tanh",
-            static_cast<Type(*)(Type)>(std::tanh)
-        );
-        set<Function>("asinh",
-            static_cast<Type(*)(Type)>(std::asinh)
-        );
-        set<Function>("acosh",
-            static_cast<Type(*)(Type)>(std::acosh)
-        );
-        set<Function>("atanh",
-            static_cast<Type(*)(Type)>(std::atanh)
-        );
-
-        set<Function>("erf",
-            static_cast<Type(*)(Type)>(std::erf)
-        );
-        set<Function>("erfc",
-            static_cast<Type(*)(Type)>(std::erfc)
-        );
-        set<Function>("tgamma",
-            static_cast<Type(*)(Type)>(std::tgamma)
-        );
-        set<Function>("lgamma",
-            static_cast<Type(*)(Type)>(std::lgamma)
-        );
-
-        set<Operator>("+",
-            [](Type x1, Type x2) noexcept { return x1 + x2; },
-            "id", std::size_t{3333}, Operator::Associativity::BOTH
-        );
-        set<Operator>("-",
-            [](Type x1, Type x2) noexcept { return x1 - x2; },
-            "neg", std::size_t{3333}, Operator::Associativity::LEFT
-        );
-        set<Operator>("*",
-            [](Type x1, Type x2) noexcept { return x1 * x2; },
-            "", std::size_t{6666}, Operator::Associativity::BOTH
-        );
-        set<Operator>("/",
-            [](Type x1, Type x2) noexcept { return x1 / x2; },
-            "", std::size_t{6666}, Operator::Associativity::LEFT
-        );
-        set<Operator>("%",
-            static_cast<Type(*)(Type, Type)>(std::fmod),
-            "", std::size_t{6666}, Operator::Associativity::LEFT
-        );
-        set<Operator>("^",
-            static_cast<Type(*)(Type, Type)>(std::pow),
-            "", std::size_t{9999}, Operator::Associativity::RIGHT
-        );
+        operators.insert({
+            {
+                "+",
+                {
+                    [](Type x1, Type x2) noexcept { return x1 + x2; },
+                    "id",
+                    std::size_t{3333},
+                    Operator::Associativity::BOTH
+                }
+            },
+            {
+                "-",
+                {
+                    [](Type x1, Type x2) noexcept { return x1 - x2; },
+                    "neg",
+                    std::size_t{3333},
+                    Operator::Associativity::LEFT
+                }
+            },
+            {
+                "*",
+                {
+                    [](Type x1, Type x2) noexcept { return x1 * x2; },
+                    "",
+                    std::size_t{6666},
+                    Operator::Associativity::BOTH
+                }
+            },
+            {
+                "/",
+                {
+                    [](Type x1, Type x2) noexcept { return x1 / x2; },
+                    "",
+                    std::size_t{6666},
+                    Operator::Associativity::LEFT
+                }
+            },
+            {
+                "%",
+                {
+                    static_cast<Type(*)(Type, Type)>(std::fmod),
+                    "",
+                    std::size_t{6666},
+                    Operator::Associativity::LEFT
+                }
+            },
+            {
+                "^",
+                {
+                    static_cast<Type(*)(Type, Type)>(std::pow),
+                    "",
+                    std::size_t{9999},
+                    Operator::Associativity::RIGHT
+                }
+            }
+        });
     }
 };
 
 
-class ComplexParser : public BaseParser<std::complex<double>> {};
+/*class ComplexParser : public BaseParser<std::complex<double>> {};
 
 class DefaultComplexParser : public ComplexParser {
 public:
@@ -331,7 +267,7 @@ public:
             "", std::size_t{9999}, Operator::Associativity::RIGHT
         );
     }
-};
+};*/
 
 }
 
