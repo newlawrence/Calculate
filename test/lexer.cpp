3f608fa01caa157b3f6199e963d7202d984a5471
Alberto Lorenzo Márquez <alorenzo.md@gmail.com>
2018-02-21 16:54:54 +0100

    Coverage enabled
    lcov is buggy under Windows

--- test/lexer.cpp
+++ test/lexer.cpp
@@ -1,2 +1,14 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+#include "calculate.hpp"
+
+
+SCENARIO( "Testing default lexer", "[default_lexer]" ) {
+
+    GIVEN( "a default lexer" ) {
+        REQUIRE_NOTHROW( calculate::Lexer<double>{} );
+        auto lexer = calculate::Lexer<double>{};
+    }
+
+}
